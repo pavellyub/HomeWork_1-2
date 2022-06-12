15f8d1f1db14a15e93da03342239ae71f5f84157
Pavel <pavel.lubushkin@gmail.com>
2022-06-12 18:34:13 +0500

    Fourth Programm

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите ваше число (положительное): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+    Console.WriteLine("Вы ввели неверное число");
+
+int count = 2;
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
